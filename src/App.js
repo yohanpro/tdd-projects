bcab7fb7fb2ab609bbf64506ab4bc5e67ae1af22
Yohan <yohan@yohanpro.com>
2020-11-30 23:21:16 +0900

    btn simulate onIncrease

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const increaseHandler = () => setCount(count => count += 1);
   return (
     <div data-test="component-app">
+      <div>
+        <h3 data-test="component-counter">counter:
+        <span data-test="component-count">{count}</span>
+        </h3>
 
+      </div>
+      <div className="button-container" data-test="component-btnContainer">
+        <button
+          className="btn btn-increase"
+          data-test="component-btnIncrease"
+          onClick={() => setCount(count + 1)}>더하기
+          </button>
+        <button
+          className="btn btn-decrease"
+          data-test="component-btnDecrease">빼기
+          </button>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
+
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = () => shallow(<App />);
+const findElement = (wrapper, value) => wrapper.find(`[data-test='${value}']`);
+
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findElement(wrapper, 'component-app');
   expect(appComponent.length).toBe(1);
 });
 
+
+test("renders counter component", () => {
+  const wrapper = setup();
+  const counterComponent = findElement(wrapper, 'component-counter');
+  expect(counterComponent.length).toBe(1);
+});
 test("renders button", () => {
+  const wrapper = setup();
+  const btnComponent = findElement(wrapper, 'component-btnContainer');
+
+
+  expect(btnComponent.children().length).toBe(2);
 
 });
-test('renders counter display', () => {
 
-});
 
 test("counter starts at 0", () => {
+  const wrapper = setup();
+  const countComponent = findElement(wrapper, 'component-count');
 
+
+  expect(countComponent.text()).toBe("0");
 });
 
-test("increment 버튼 클릭시 ", () => {
+test("increment 버튼 클릭시 counter 증가", () => {
 
+  const wrapper = setup();
+  const btnIncrease = findElement(wrapper, 'component-btnIncrease');
+  btnIncrease.simulate('click');
+  const countComponent = findElement(wrapper, 'component-count').text();
+  expect(countComponent).toBe("1");
 });
