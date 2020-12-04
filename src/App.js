ef488e276ff09db697ba789dc9344bf6f15f54f7
Yohan <yohan@yohanpro.com>
2020-12-04 21:24:47 +0900

    click counter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,64 @@
-
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
+    this.state = {
+      counter: 0,
+      error: false,
+    };
 
-  const [count, setCount] = useState(0);
+    // need to bind this for decrementCounter and incrementCounter, since they use
+    // this.state and this.setState
+    this.decrementCounter = this.decrementCounter.bind(this);
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+  // for challenge #2, the behavior for decrement is complicated enough
+  // that I separated the function from the jsx.
+  decrementCounter() {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  }
 
-  const increaseHandler = () => setCount(count => count += 1);
-  return (
-    <div data-test="component-app">
-      <div>
-        <h3 data-test="component-counter">counter:
-        <span data-test="component-count">{count}</span>
-        </h3>
+  // for challenge #3, I separated the incrementCounter onClick
+  incrementCounter() {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  }
 
+  render() {
+    // determine whether error is hidden based on state
+    const errorClass = this.state.error ? '' : 'hidden';
+
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <div data-test="error-message" className={`error ${errorClass}`}>
+          The counter cannot go below 0
       </div>
-      <div className="button-container" data-test="component-btnContainer">
         <button
-          className="btn btn-increase"
-          data-test="component-btnIncrease"
-          onClick={() => setCount(count + 1)}>더하기
-          </button>
+          data-test="increment-button"
+          onClick={this.incrementCounter}
+        >
+          Increment counter
+      </button>
         <button
-          className="btn btn-decrease"
-          data-test="component-btnDecrease">빼기
-          </button>
+          data-test="decrement-button"
+          onClick={this.decrementCounter}
+        >
+          Decrement counter
+      </button>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
